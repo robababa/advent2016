4f249354cf484256692dd607804776a8e5e5124b
Rob Johnson <robjohnsonofwichita@hotmail.com>
2016-12-04 13:54:40 -0600

    Day 3 part 1 works

--- src/day3.js
+++ src/day3.js
@@ -1,29 +1,53 @@
 import React, { Component } from 'react';
-import { SHORT_LIST as INITIAL_LIST } from './day3_data';
+import { FULL_LIST as INITIAL_LIST } from './day3_data';
+
+const BATCH_SIZE = 100;
 
 export default class Day3 extends Component {
   constructor() {
     super();
     this.state = {
-      validTriangleCount: 0,
+      batchRunCount: 0,
+      totalRunCount: 0,
+      validCount: 0,
       triangleList: INITIAL_LIST,
-      history: []
+      history: [`(validCount: 0, triangleList: ${INITIAL_LIST})`]
     }
   }
 
   componentDidUpdate(prevProps, prevState) {
     // keep going if we have more triangle candidates to inspect
-    if (this.state.triagleList.length > 0) {
+    if ((this.state.triangleList.length > 0) &&
+        (this.state.batchRunCount !== 0)) {
       this.tick();
     }
   }
 
   tick() {
-    this.setState({
-      validTriangleCount: this.state.validTriangleCount + 1,
-      triangleList:  this.state.triangleList.slice(1),
-      history: this.state.history
-    });
+    if (this.state.triangleList.length > 0) {
+      var nextValid = this.isValidTriangle(this.state.triangleList[0]);
+      var nextBatchRunCount = (this.state.batchRunCount + 1) % BATCH_SIZE;
+      var nextTotalRunCount = this.state.totalRunCount + 1;
+      var nextValidCount = this.state.validCount + nextValid;
+      var nextTriangleList = this.state.triangleList.slice(1);
+      this.setState({
+        batchRunCount: nextBatchRunCount,
+        totalRunCount: nextTotalRunCount,
+        validCount: nextValidCount,
+        triangleList:  nextTriangleList,
+        history: this.state.history
+//        history: [
+//          `(validCount: ${nextValidCount}, triangleList: ${nextTriangleList})`,
+//          ...this.state.history
+//        ]
+      });
+    }
+  }
+
+  isValidTriangle(triangleToCheck) {
+    var perimeter = triangleToCheck.reduce(function(a,b) { return a + b; }, 0);
+    var maxSide = Math.max(...triangleToCheck);
+    return (maxSide * 2 < perimeter) ? 1 : 0;
   }
 
 
@@ -42,7 +66,9 @@ export default class Day3 extends Component {
         <h3>Day 3</h3>
         <p onClick={() => this.tick()}>Click me to run!</p>
         <p><code>Current State:</code></p>
-        <p><code>validTriangleCount: {this.state.validTriangleCount}</code></p>
+        <p><code>batchRunCount: {this.state.batchRunCount}</code></p>
+        <p><code>totalRunCount: {this.state.totalRunCount}</code></p>
+        <p><code>validCount: {this.state.validCount}</code></p>
         <p><code>triangleList: {this.displayTriangleList()}</code></p>
         <code><pre>history: {this.displayHistory()}</pre></code>
       </div>
