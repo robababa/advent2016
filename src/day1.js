901934b11f1c58ec8c47ffc3e8619e0c562a3f85
Rob Johnson <robjohnsonofwichita@hotmail.com>
2016-12-03 23:35:12 -0600

    Get right answer for part 2 example

--- src/day1.js
+++ src/day1.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
-//const DIRECTIONS="R1, R3, L1, L10";
+const DIRECTIONS="R8, R4, R4, R8";
 
-const DIRECTIONS="R3, L2, L2, R4, L1, R2, R3, R4, L2, R4, L2, L5, L1, R5, R2, R2, L1, R4, R1, L5, L3, R4, R3, R1, L1, L5, L4, L2, R5, L3, L4, R3, R1, L3, R1, L3, R3, L4, R2, R5, L190, R2, L3, R47, R4, L3, R78, L1, R3, R190, R4, L3, R4, R2, R5, R3, R4, R3, L1, L4, R3, L4, R1, L4, L5, R3, L3, L4, R1, R2, L4, L3, R3, R3, L2, L5, R1, L4, L1, R5, L5, R1, R5, L4, R2, L2, R1, L5, L4, R4, R4, R3, R2, R3, L1, R4, R5, L2, L5, L4, L1, R4, L4, R4, L4, R1, R5, L1, R1, L5, R5, R1, R1, L3, L1, R4, L1, L4, L4, L3, R1, R4, R1, R1, R2, L5, L2, R4, L1, R3, L5, L2, R5, L4, R5, L5, R3, R4, L3, L3, L2, R2, L5, L5, R3, R4, R3, R4, R3, R1";
+//const DIRECTIONS="R3, L2, L2, R4, L1, R2, R3, R4, L2, R4, L2, L5, L1, R5, R2, R2, L1, R4, R1, L5, L3, R4, R3, R1, L1, L5, L4, L2, R5, L3, L4, R3, R1, L3, R1, L3, R3, L4, R2, R5, L190, R2, L3, R47, R4, L3, R78, L1, R3, R190, R4, L3, R4, R2, R5, R3, R4, R3, L1, L4, R3, L4, R1, L4, L5, R3, L3, L4, R1, R2, L4, L3, R3, R3, L2, L5, R1, L4, L1, R5, L5, R1, R5, L4, R2, L2, R1, L5, L4, R4, R4, R3, R2, R3, L1, R4, R5, L2, L5, L4, L1, R4, L4, R4, L4, R1, R5, L1, R1, L5, R5, R1, R1, L3, L1, R4, L1, L4, L4, L3, R1, R4, R1, R1, R2, L5, L2, R4, L1, R3, L5, L2, R5, L4, R5, L5, R3, R4, L3, L3, L2, R2, L5, L5, R3, R4, R3, R4, R3, R1";
 
 export default class Day1 extends Component {
   constructor() {
@@ -11,6 +11,8 @@ export default class Day1 extends Component {
       direction: 'N',
       xPosition: 0,
       yPosition: 0,
+      visitedPositions: ["(0,0)"],
+      firstVisitedTwice: null,
       instructions: DIRECTIONS.split(", "),
       history: []
     }
@@ -75,20 +77,56 @@ export default class Day1 extends Component {
     }
   }
 
+  determineTwice(newPlaces) {
+    if (this.state.firstVisitedTwice != null) {
+      return this.state.firstVisitedTwice;
+    }
+    console.log(newPlaces);
+    // we haven't found a twice-visited place yet, so look through the new ones
+    for (var i=0; i<newPlaces.length; i++) {
+      console.log(this.state.visitedPositions, newPlaces[i]);
+      if (this.state.visitedPositions.indexOf(newPlaces[i]) !== -1) {
+        return newPlaces[i];
+      }
+    }
+
+    // didn't find one, so return null
+    return null;
+  }
+
+  computeNextPlaces(nextDirection, nextJump) {
+    var newPlaces = [];
+    for (var i = 1; i<= nextJump; i++) {
+      if (nextDirection === 'E') {
+        newPlaces = [ `(${this.state.xPosition + i},${this.state.yPosition})`, ...newPlaces ];
+      } else if (nextDirection === 'W') {
+        newPlaces = [ `(${this.state.xPosition - i},${this.state.yPosition})`, ...newPlaces ];
+      } else if (nextDirection === 'N') {
+        newPlaces = [ `(${this.state.xPosition},${this.state.yPosition + i})`, ...newPlaces ];
+      } else if (nextDirection === 'S') {
+        newPlaces = [ `(${this.state.xPosition},${this.state.yPosition - i})`, ...newPlaces ];
+      }
+    }
+    return newPlaces;
+  }
+
   advanceOne() {
     var nextInstruction = this.state.instructions[0];
     var nextDirection = this.newDirection(this.state.direction, nextInstruction);
     var nextJump = this.jumpSize(nextInstruction);
     var nextX = this.newXPosition(nextDirection, nextJump);
     var nextY = this.newYPosition(nextDirection, nextJump);
+    var newPlaces = this.computeNextPlaces(nextDirection, nextJump);
     var archivedState = this.condensedState();
     // set the next state
     this.setState({
       direction: nextDirection,
       xPosition: nextX,
       yPosition: nextY,
+      visitedPositions: [ ...newPlaces, ...this.state.visitedPositions ],
+      firstVisitedTwice: this.determineTwice(newPlaces, this.visitedPositions),
       instructions: this.state.instructions.slice(1),
-      history: [archivedState, this.state.history]
+      history: [archivedState, ...this.state.history]
     });
   }
 
@@ -101,6 +139,8 @@ export default class Day1 extends Component {
         <p><code>direction: {this.state.direction}</code></p>
         <p><code>xPosition: {this.state.xPosition}</code></p>
         <p><code>yPosition: {this.state.yPosition}</code></p>
+        <p><code>firstVisitedTwice: {this.state.firstVisitedTwice}</code></p>
+        <p><code>visitedPositions: {this.state.visitedPositions}</code></p>
         <p><code>instructions: {this.state.instructions}</code></p>
         <code>history: {this.state.history}</code>
       </div>
