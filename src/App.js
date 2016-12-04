5c763fd6bab119e3c52c46b5551fa83e09e5ecec
Rob Johnson <robjohnsonofwichita@hotmail.com>
2016-12-04 10:35:27 -0600

    Day 2 initial display works
    I still need to fill in the state transitions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Day1 from './day1';
+import Day2 from './day2';
 
 class App extends Component {
   render() {
@@ -15,6 +16,9 @@ class App extends Component {
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
+        </div>
+        <div>
+          <Day2 />
         </div>
         <div>
           <Day1 />
---
+++ src/day2.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+const BATHROOM_CODE="ULL\nRRDDD\nLURDL\nUUUUD"
+
+export default class Day2 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentKey: '5',
+      instructions: BATHROOM_CODE.split("\n"),
+      history: []
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.state.instructions.length > 0) {
+      this.tick();
+    }
+  }
+
+  tick() {
+    // we only do work if there are more instructions to follow
+    if (this.state.instructions.length > 0)
+    {
+      this.advanceOne();
+    }
+  }
+
+  advanceOne() {
+    this.setState({
+      currentKey: this.state.currentKey,
+      instructions: this.state.instructions,
+      history: this.state.history
+    });
+  }
+
+  displayInstructions() {
+    return this.state.instructions.join(",")
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Day 2</h3>
+        <p onClick={() => this.tick()}>Click me to run!</p>
+        <p><code>Current State:</code></p>
+        <p><code>currentKey: {this.state.currentKey}</code></p>
+        <p><code>instructions: {this.displayInstructions()}</code></p>
+        <code>history: {this.state.history}</code>
+      </div>
+    );
+  }
+}
+
