90251580b43f902a97d0daf25af23e4a7b582a0b
Rob Johnson <robjohnsonofwichita@hotmail.com>
2016-12-14 00:38:17 -0600

    Day 14 part 2 solved!

--- nonreact/day14.sql
+++ nonreact/day14.sql
@@ -1,17 +1,33 @@
-drop table md5s, md5_triples, md5_quintuples cascade;
+drop table if exists md5s, md5_triples, md5_quintuples cascade;
 
 create table md5s (num int not null primary key, hash varchar(32) not null);
+
+-- part 2
+-- run these commands once
+--create or replace function md5_2 (t text) returns text as $$ select md5(md5(t)); $$ language sql;
+--create or replace function md5_4 (t text) returns text as $$ select md5_2(md5_2(t)); $$ language sql;
+--create or replace function md5_8 (t text) returns text as $$ select md5_4(md5_4(t)); $$ language sql;
+--create or replace function md5_16 (t text) returns text as $$ select md5_8(md5_8(t)); $$ language sql;
+--create or replace function md5_32 (t text) returns text as $$ select md5_16(md5_16(t)); $$ language sql;
+--create or replace function md5_64 (t text) returns text as $$ select md5_32(md5_32(t)); $$ language sql;
+--create or replace function md5_128 (t text) returns text as $$ select md5_64(md5_64(t)); $$ language sql;
+--create or replace function md5_256 (t text) returns text as $$ select md5_128(md5_128(t)); $$ language sql;
+--create or replace function md5_512 (t text) returns text as $$ select md5_256(md5_256(t)); $$ language sql;
+--create or replace function md5_1024 (t text) returns text as $$ select md5_512(md5_512(t)); $$ language sql;
 
 insert into md5s
 with
 source
 as
 (
-  select generate_series(1, 500000, 1) as num
+  select generate_series(1, 50000, 1) as num
 )
 select
 num,
-md5('yjdafjpo' || num::varchar)
+-- part 1
+--md5(md5(md5('yjdafjpo' || num::varchar)))
+-- part 2
+md5_1024(md5_512(md5_256(md5_128(md5_64(md5_32(md5('yjdafjpo' || num::varchar)))))))
 from
 source;
 
