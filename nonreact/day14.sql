7b57166e62ba420c6187cc228bc5f365ef89754f
Rob Johnson <robjohnsonofwichita@hotmail.com>
2016-12-13 23:55:29 -0600

    This does not work, but feels like it should

--- nonreact/day14.sql
+++ nonreact/day14.sql
@@ -5,3 +5,20 @@ insert into md5s with source as (select generate_series(1, 500000, 1) as num) se
 create table md5_triples as select * from md5s where hash ~ E'(.)\\1\\1';
 
 create table md5_quintuples as select * from md5_triples where hash ~ E'(.)\\1\\1\\1\\1';
+
+select
+t.num,
+t.hash,
+(regexp_matches(t.hash, E'(\\w)\\1\\1'))[1] as repeat_char,
+q.num,
+q.hash
+from
+md5_triples as t,
+md5_quintuples as q
+where
+(regexp_matches(t.hash, E'(\\w)\\1\\1'))[1] =
+(regexp_matches(q.hash, E'(\\w)\\1\\1\\1\\1'))[1]
+and
+q.num between (t.num + 1) and (t.num + 1000)
+order by
+t.num, q.num;
