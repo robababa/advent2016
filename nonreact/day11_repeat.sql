6c32cfcb864544c81b2991980ec5d5d5ef3a0203
Rob Johnson <robjohnsonofwichita@hotmail.com>
2016-12-15 21:06:13 -0600

    rename building to position and fix a typo
    The code works on the sample data, but the last step takes almost two
    minutes to run.  That makes me think my algorithm is too inefficient to
    solve the real problem.

--- nonreact/day11_repeat.sql
+++ nonreact/day11_repeat.sql
@@ -1,20 +1,20 @@
 -- do this repeatedly until either
--- (1) we connect the two buildings we want
--- (2) we aren't adding any more buildings to the connected table
-insert into connected (building1_id, building2_id, distance)
+-- (1) we connect the two positions we want
+-- (2) we aren't adding any more positions to the connected table
+insert into connected (position1_id, position2_id, distance)
 with
 first_candidates
 as
 (
   select
-  least(c1.building1_id, c2.building1_id) as building1_id,
-  greatest(c1.building1_id, c2.building1_id) as building2_id,
+  least(c1.position1_id, c2.position1_id) as position1_id,
+  greatest(c1.position1_id, c2.position1_id) as position2_id,
   c1.distance + c2.distance as new_distance
   from
   connected as c1 inner join
   connected as c2
   on
-  c1.building2_id = c2.building2_id -- the two connections have a common endbuilding
+  c1.position2_id = c2.position2_id -- the two connections have a common endposition
   where
   c1.id <> c2.id -- the connections are distinct
 ),
@@ -22,14 +22,14 @@ second_candidates
 as
 (
   select
-  least(c1.building2_id, c2.building2_id) as building1_id,
-  greatest(c1.building2_id, c2.building2_id) as building2_id,
+  least(c1.position2_id, c2.position2_id) as position1_id,
+  greatest(c1.position2_id, c2.position2_id) as position2_id,
   c1.distance + c2.distance
   from
   connected as c1 inner join
   connected as c2
   on
-  c1.building1_id = c2.building1_id -- the two connections have a common endbuilding
+  c1.position1_id = c2.position1_id -- the two connections have a common endposition
   where
   c1.id <> c2.id -- the connections are distinct
 ),
@@ -37,14 +37,14 @@ third_candidates
 as
 (
   select
-  least(c1.building2_id, c2.building1_id) as building1_id,
-  greatest(c1.building2_id, c2.building1_id) as building2_id,
+  least(c1.position2_id, c2.position1_id) as position1_id,
+  greatest(c1.position2_id, c2.position1_id) as position2_id,
   c1.distance + c2.distance
   from
   connected as c1 inner join
   connected as c2
   on
-  c1.building1_id = c2.building2_id -- the two connections have a common endbuilding
+  c1.position1_id = c2.position2_id -- the two connections have a common endposition
   where
   c1.id <> c2.id -- the connections are distinct
 ),
@@ -52,14 +52,14 @@ fourth_candidates
 as
 (
   select
-  least(c1.building1_id, c2.building2_id) as building1_id,
-  greatest(c1.building1_id, c2.building2_id) as building2_id,
+  least(c1.position1_id, c2.position2_id) as position1_id,
+  greatest(c1.position1_id, c2.position2_id) as position2_id,
   c1.distance + c2.distance
   from
   connected as c1 inner join
   connected as c2
   on
-  c1.building2_id = c2.building1_id -- the two connections have a common endbuilding
+  c1.position2_id = c2.position1_id -- the two connections have a common endposition
   where
   c1.id <> c2.id -- the connections are distinct
 ),
@@ -72,26 +72,26 @@ as
   select * from fourth_candidates
 )
 select
-distinct on (cc.building1_id, cc.building2_id)
-cc.building1_id, cc.building2_id, cc.new_distance
+distinct on (cc.position1_id, cc.position2_id)
+cc.position1_id, cc.position2_id, cc.new_distance
 from
 combined_candidates as cc left join connected as c
 on
-cc.building1_id = c.building1_id and
-cc.building2_id = c.building2_id
+cc.position1_id = c.position1_id and
+cc.position2_id = c.position2_id
 where
-c.building1_id is null
+c.position1_id is null
 order by
-cc.building1_id, cc.building2_id, cc.new_distance;
+cc.position1_id, cc.position2_id, cc.new_distance;
 
 -- do we have a result?
 select c.*
 from
-buildings as p1
+position as p1
 inner join
-connected as c on p1.id = c.building1_id
+connected as c on p1.id = c.position1_id
 inner join
-buildings as p2 on c.building2_id = p2.id
+position as p2 on c.position2_id = p2.id
 where
 (p1.e = 1 and p1.m1 = 1 and p1.g1 = 2 and p1.m2 = 1 and p1.g2 = 3)
 and
@@ -99,13 +99,13 @@ and
 
 -- part 2
 -- select
--- /* building (1,1 itself) adds one */
+-- /* position (1,1 itself) adds one */
 -- count(*) + 1
--- from buildings as p,
+-- from positions as p,
 -- connected as c
 -- where
 -- p.x = 1 and p.y = 1
 -- and
--- (p.id = c.building1_id or p.id = c.building2_id)
+-- (p.id = c.position1_id or p.id = c.position2_id)
 -- and
 -- c.distance <= 50;
