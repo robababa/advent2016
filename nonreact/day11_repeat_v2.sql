cea9db511a05a40d62f5ae2ef45c9b3b350eb020
Rob Johnson <robjohnsonofwichita@hotmail.com>
2016-12-15 23:25:39 -0600

    Try a new method.  It doesn't work yet.
    Here we add a "sprawl" table, and sprawl out from our original puzzle
    position, finding all possible next positions, until one of those paths
    reaches our puzzle destination.  Right now, it works for a few
    iterations, then stops finding any new positions.  The repeat script
    should run 11 times to find the destination.

---
+++ nonreact/day11_repeat_v2.sql
@@ -0,0 +1,31 @@
+-- do this repeatedly until we get the answer
+
+insert into sprawl
+(position_id, distance)
+select
+source.position_id, source.new_distance
+from
+(
+select
+c.position2_id as position_id, s1.distance + 1 as new_distance
+from
+connected c
+inner join sprawl as s1 on
+position1_id = s1.position_id
+UNION
+select
+c.position1_id as position_id, s2.distance + 1 as new_distance
+from
+connected c inner join sprawl as s2 on
+position1_id = s2.position_id
+) as source
+left join
+sprawl as already_sprawl
+on
+source.position_id = already_sprawl.position_id
+where
+already_sprawl.position_id is null;
+
+-- look for our destination row
+--select * from sprawl where position_id = 100000000;
+
