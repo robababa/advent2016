334e2c647d999903112e0dcc577e45506a187a6b
Rob Johnson <robjohnsonofwichita@hotmail.com>
2016-12-17 17:43:01 -0600

    Show the solution path.

---
+++ nonreact/day11_show_solution.sql
@@ -0,0 +1,24 @@
+with recursive
+sprawl_positions(position_id, distance) as (
+select position_id, distance from sprawl where position_id = 100000000
+UNION ALL
+select
+s.position_id, s.distance
+from
+sprawl_positions as sp
+inner join
+outreach as o
+on sp.position_id = o.id_new
+inner join
+sprawl as s
+on o.id_old = s.position_id
+where
+s.distance = sp.distance - 1
+)
+select
+sprawl_positions.distance,
+position.m_code,
+position.g_code
+from sprawl_positions inner join position
+on sprawl_positions.position_id = position.id
+order by sprawl_positions.distance;
