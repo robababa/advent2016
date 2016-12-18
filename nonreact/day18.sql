5768c9a2fd03e005560815be8ba2ebcd9dc2ff8d
Rob Johnson <robjohnsonofwichita@hotmail.com>
2016-12-18 13:52:49 -0600

    Day 18 works for part 2
    Took 101 seconds

--- nonreact/day18.sql
+++ nonreact/day18.sql
@@ -1,6 +1,6 @@
 with recursive
 tiles (tile_row, tile_row_number) as (
-  select '.^^.^.^^^^', 1
+  select '.^^.^^^..^.^..^.^^.^^^^.^^.^^...^..^...^^^..^^...^..^^^^^^..^.^^^..^.^^^^.^^^.^...^^^.^^.^^^.^.^^.^.', 1
 UNION ALL
   (
     with
@@ -28,78 +28,10 @@ UNION ALL
       from
       source2
       having
-      min(tile_row_number) < min(tile_row_length)
+      -- min(tile_row_number) < min(tile_row_length)
+      min(tile_row_number) < 400000
   )
 )
 -- for a simple tile display, just
--- select * from tiles
-select sum(length(replace(tile_row, '^', ''))) from tiles;
-
-
--- select tiles.tile_row, tiles.tile_row_number + 1
--- from
--- tiles
--- where
--- tiles.tile_row_number < length(tiles.tile_row)
--- )
 -- select * from tiles;
-
-/*
-with recursive
-tiles (tile_row, tile_row_number) as (
-select '..^^.', 1
-UNION ALL
-select tiles.tile_row, tiles.tile_row_number + 1
-from
-tiles
-where
-tiles.tile_row_number < length(tiles.tile_row)
-)
-select * from tiles;
-
-with
-source as (select '..^^.'::text as s),
-source2 as
-(
-  select
-  substring('.' || s || '.', generate_series(1, length(s), 1), 3)
-    as tiles_above
-  from
-  source
-)
-select
-string_agg(
-  case
-    when tiles_above in ('^^.','^..','..^','.^^')
-    then '^'
-    else '.'
-  end,
-  ''
-)
-from
-source2;
-*/
-
----- with
----- source
-----   as (select generate_series(0, 17 - 1, 1) as offset),
----- source2
----- as
----- (
----- select
----- case
-----   length(
-----       replace(
-----             substring(source_data, 1 + source.offset*2*1024*1024, 2*1024*1024)::varchar,
-----                 '0',''
-----                   )
-----                 ) % 2
----- when 0 then '1'
----- else '0'
----- end as checksum_char
----- from
----- source
----- cross join
----- data
----- order by source)
----- select string_agg(source2.checksum_char, '') from source2;
+select sum(length(replace(tile_row, '^', ''))) from tiles;
