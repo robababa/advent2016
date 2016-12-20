31ff30925c952b319fb11e9b86cc0401eaff976a
Rob Johnson <robjohnsonofwichita@hotmail.com>
2016-12-19 23:28:34 -0600

    Tried to make it more efficient, but I don't think it is.

--- nonreact/day19_part2.js
+++ nonreact/day19_part2.js
@@ -1,7 +1,11 @@
 var elves = 3004953;
 var half = (elves - 1) / 2;
 var current_source = Array(elves).fill().map((x,i)=>((i+half)%elves + 1));
+var current_length = elves;
+var current_elf = 0;
+
 var other_source = [];
+
 var discard = 0;
 var last = 0;
 var cycle = 0;
@@ -20,11 +24,13 @@ while (current_source.length > 0) {
     other_source.push(last);
   }
   cycle = (cycle + 1) % 3;
+  current_elf += 1;
 
-  // if we've exhausted the current list of numbers, switch over to the
-  // other list
-  if (! current_source[0]) {
-    [current_source, other_source] = [other_source, current_source];
+  // if we've exhausted the current list of numbers, then swap lists
+  if (current_elf >= current_length) {
+    [current_source, other_source] = [other_source, []];
+    current_elf = 0;
+    current_length = current_source.length;
   }
 
 }
