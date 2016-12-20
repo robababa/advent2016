0f895424c3c5cf1852f51a74bd66af0bea8adb43
Rob Johnson <robj@hightouchinc.com>
2016-12-20 10:03:38 -0600

    I can reduce the ranges once.
    Need to do it until they can't be reduced anymore, then take the sum of
    their lengths and subtract that from 4294967296.

--- nonreact/day20_solve.sql
+++ nonreact/day20_solve.sql
@@ -1,36 +1,67 @@
--- part 1
-with
-candidates as (
-  -- it would be 0 if no range contained zero
-  select 0
-  union all
-  select upper(blocked) as candidate from blocked_range
-  order by 1
-)
-select
-min(candidate)
-from
-candidates
-where
-not exists(
-  select 1 from blocked_range where blocked @> candidate
-);
+-- -- part 1l
+-- with
+-- candidates as (
+--   -- it would be 0 if no range contained zero
+--   select 0
+--   union all
+--   select upper(blocked) as candidate from blocked_range
+--   order by 1
+-- )
+-- select
+-- min(candidate)
+-- from
+-- candidates
+-- where
+-- not exists(
+--   select 1 from blocked_range where blocked @> candidate
+-- );
 
 -- part 2
-with source as (
-  select '[1,4]'::int8range as blocked UNION ALL
-  select '[2,5]'::int8range UNION ALL
-  select '[7,10]'::int8range UNION ALL
-  select '[11, 13]'::int8range
-  order by 1
+
+drop aggregate if exists sum(int8range);
+
+create aggregate sum(int8range) (sfunc = range_union, stype = int8range, initcond = 'empty');
+
+with
+source as (
+  select
+  blocked,
+  row_number() over (order by blocked) as starting_segment
+  from
+  blocked_range
+  order by 2
+),
+segment_count as (
+  select count(*) as segment_count from source
+),
+identify_merges as (
+  select
+  blocked,
+  starting_segment,
+  case
+    -- when this blocked range overlaps (&&) or is adjacent to (-|-) the
+    -- preceding range, then we can merge it.
+    when (blocked && lag(blocked) over () or blocked -|- lag(blocked) over ())
+    then null::int8
+  else starting_segment
+  end
+    as merge_segment
+  from source
+  order by blocked
+),
+identify_final_segments as (
+  select
+  blocked,
+  starting_segment,
+  merge_segment,
+  max(merge_segment) over (rows unbounded preceding) as final_segment
+  from
+  identify_merges
+  order by starting_segment
 )
 select
-blocked,
-case
-  when (blocked && lag(blocked) over () or blocked -|- lag(blocked) over ())
-  then true
-  else false
-end
-  as merge_it
-from source
-order by blocked;
+sum(blocked) as blocked,
+final_segment as starting_segment
+from identify_final_segments
+group by
+final_segment;
