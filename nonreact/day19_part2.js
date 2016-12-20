f915242728434bcc387a0a6327365595d6473cc7
Rob Johnson <robj@hightouchinc.com>
2016-12-20 08:30:34 -0600

    Use Array.filter, went from 5.5 hrs to 2 secs
    Really.

--- nonreact/day19_part2.js
+++ nonreact/day19_part2.js
@@ -1,3 +1,10 @@
+// since the number of elves is odd, we know that after the first elf
+// removal, the next elf will be skipped
+// if the number of elves was even, then that next elf would be removed,
+// then the following elf skipped
+// So this isn't a general solution, but I've already spent more time on this
+// than intended, so I wil leave it like this
+
 var elves = 3004953;
 var half = (elves - 1) / 2;
 var current_source = Array(elves).fill().map((x,i)=>((i+half)%elves + 1));
@@ -8,31 +15,25 @@ var other_source = [];
 
 var discard = 0;
 var last = 0;
-var cycle = 0;
+var cycle = -1; // we add 1 before the filter, so prime it to be one less
 
 //console.log(current_source);
 //console.log(other_source);
 
-while (current_source.length > 0) {
-  // we remove two of every three elements in circular fashion
-  if ((cycle % 3 === 0) || (cycle % 3 === 2)) {
-    discard = current_source.shift();
-    //console.log(`REMOVING ${discard}`);
-  } else {
-    last = current_source.shift();
-    //console.log(`saving ${last}`);
-    other_source.push(last);
-  }
-  cycle = (cycle + 1) % 3;
-  current_elf += 1;
+while (current_length > 0) {
+  other_source = current_source.filter(function (element) {
+    cycle = (cycle + 1) % 3;
+    if (cycle % 3 === 1) {
+      last = element;
+      return true;
+    } else {
+      return false;
+    }
+  });
 
-  // if we've exhausted the current list of numbers, then swap lists
-  if (current_elf >= current_length) {
-    [current_source, other_source] = [other_source, []];
-    current_elf = 0;
-    current_length = current_source.length;
-  }
-
+  // now swap arrays
+  [current_source, other_source] = [other_source, []];
+  current_length = current_source.length;
 }
 
 console.log(`Winner is ${last}`);
